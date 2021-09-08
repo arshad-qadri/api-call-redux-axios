571265b5d82c634c3ba8cffca7d2074dfddc7094
arshad-qadri <arshadqadri321@gmail.com>
2021-09-08 12:44:06 +0530

    changes in action

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,8 +3,20 @@
 import axios from "axios";
 
 export const getData = () => {
-  return async (dispatch) => {
-    const api = await axios("https://jsonplaceholder.typicode.com/users");
-    dispatch({ type: "GET_DATA", payload: api });
+  //////////////////////////// Both are working ///////////////////////////////////
+  // return async (dispatch) => {
+  //   const api = await axios("https://jsonplaceholder.typicode.com/users");
+  //   dispatch({ type: "GET_DATA", payload: api });
+  // };
+
+  return (dispatch) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users", {})
+      .then((api) => {
+        dispatch({ type: "GET_DATA", payload: api });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
